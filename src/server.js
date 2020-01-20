eb52be9da2a20fa39282b7aa3a75ad1a20aa5f94
unknown <cswebd3v@gmail.com>
2020-01-20 11:26:01 -0600

    updated with settings for heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
-const app = require('./app')
-const { PORT } = require('./config')
+const express = require('express');
+const app = express();
+
+const PORT = process.eventNames.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
+
+module.exports = {app};
